2df5a1e8605151ec8464bcdb6b9b846ec67b4423
chelle <maphaiyarath@gmail.com>
2021-01-16 16:26:15 -0600

    added burger controller

---
+++ controllers/burgersController.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var router = express.Router();
+
+// import the model to use its database functions
+var burger = require("../models/burger.js");
+
+// create the routes and set up logic within those routes where required
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var bars = {
+            burgers: data
+        };
+        console.log(bars);
+        res.render("index", bars);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create(
+        ["burger_name", "devoured"],
+        [req.body.burger_name, req.body.devoured],
+        function(res) {
+            // send back id of new route
+            res.json({ id: res.insertId });
+        });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "ID = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.update(
+        { devoured: req.body.devoured },
+        condition,
+        function(res) {
+            if (res.changedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        }
+    );
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "ID = " + req.params.id;
+
+    burger.delete(condition, function (res) {
+        if (res.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// export those routes for server.js to use
+module.exports = router;
