328aae37bbbf7dfe31126a199dba0e25e33b734b
chelle <maphaiyarath@gmail.com>
2021-01-17 12:53:39 -0600

    added ajax logic for js interaction

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,64 @@
+// make sure we wait to attach handlers until the dom is fully loaded
+$(function() {
+    // change devoured boolean
+    $("change-devoured").on("click", function(event) {
+        var id = $(this).data("id");
+        var newDevoured = $(this).data("newdevoured");
+    
+        var newDevouredState = {
+            devoured: newDevouredState
+        };
+
+        // send put request
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function() {
+                console.log("changed devoured to ", newDevoured);
+                
+                // reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event) {
+        // make sure to prevent default on a submit event
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#burger").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        // send the post request
+        $.ajax("/api/burgers/", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("created new burger");
+                
+                // reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".delete-burger").on("click", function(event) {
+        var id = $(this).data("id");
+
+        // send the delete request
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE"
+        }).then(
+            function() {
+                console.log("deleted burger", id);
+                
+                // reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
